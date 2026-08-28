d2ec4f1ce428dff3961962341f0afa8acde186d4
yabsirahanne <yabsirahanne@gmail.com>
2026-08-28 18:12:33 +0300

    sign up form

---
+++ AdavancedHTml&css/die.js
@@ -0,0 +1,12 @@
+const selectElem = document.querySelector("select");
+const example = document.querySelector("#example-element");
+
+selectElem.addEventListener("change", () => {
+  if (selectElem.value === "Choose a function") {
+    return;
+  }
+  example.style.transform = `rotate3d(1, 1, 1, 30deg) ${selectElem.value}`;
+  setTimeout(() => {
+    example.style.transform = "rotate3d(1, 1, 1, 30deg)";
+  }, 2000);
+});
